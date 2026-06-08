15f6480923df2ac7216d5b0d82a3c23f1dc5497c
Mrlong8 <xuanlong1806@gmail.com>
2026-06-08 15:09:00 +0700

    Học cơ bản 1

---
+++ lesson1/3_HelloWorld/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let message = "Hello Word";
+console.log(message);
+const sum3 = (x, y) => {
+    return x + y;
+};
+console.log(">>> Check Sum3 : ", sum3(9, 7));
---
+++ lesson1/3_HelloWorld/main.ts
@@ -0,0 +1,10 @@
+let message: string = "Hello Word";
+console.log(message);
+
+
+
+
+const sum3 = (x: number, y: number) => {
+    return x + y
+}
+console.log(">>> Check Sum3 : ", sum3(9, 7))
